872310ade90d0e80b1e226cd79a618dddba4a0d5
Caleb McCreary <caleb@deltasigmacompany.com>
2018-11-26 13:10:03 -0500

    ADd problem 5 solution

--- ProjectEuler/Python3/problem5.py
+++ ProjectEuler/Python3/problem5.py
@@ -1,3 +1,12 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    
+    return a
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
 def smallest_multiple(limit):
     divisors = list(range(limit, 0, -1))
     smallest_multiple = 2
@@ -5,10 +14,10 @@ def smallest_multiple(limit):
         if smallest_multiple % divisors[-1] == 0:
             divisors.pop()
         else:
-            smallest_multiple *= divisors[-1]
+            smallest_multiple = lcm(smallest_multiple, divisors[-1])
 
     return smallest_multiple
 
 if __name__ == '__main__':
-    limit = 10
+    limit = 20
     print(smallest_multiple(limit))
