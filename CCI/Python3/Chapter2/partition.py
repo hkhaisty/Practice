e316988c37c6cc9cef49ef80258f7094fd0f87f3
Caleb_McCreary <clsmccreary@gmail.com>
2018-11-30 23:33:24 -0500

    Add partition

--- CCI/Python3/Chapter2/partition.py
+++ CCI/Python3/Chapter2/partition.py
@@ -1,38 +1,37 @@
 import unittest
 from linked_list import LinkedList
-from linked_list_node import LinkedListNode
 
 
 def partition(linked_list, value):
-    head = linked_list.root
-    tail = linked_list.root
-    while linked_list.root is not None:
-        if linked_list.root.value < value:
-            head.next = LinkedListNode(linked_list.root.value)
-            head = head.next
+    head = tail = current = linked_list.head
+    while current is not None:
+        next_node = current.next
+        if current.value < value:
+            current.next = head
+            head = current
         else:
-            tail.next = LinkedListNode(linked_list.root.value)
+            tail.next = current
             tail = tail.next
-        linked_list.root = linked_list.root.next
+        current = next_node
 
-    return left_partition
+    return head
 
 
 class Test(unittest.TestCase):
     values = [3, 5, 8, 5, 10, 2, 1]
     partition = 5
-    expected = [3, 2, 1, 5, 8, 5, 10]
+    expected = [1, 2, 3, 5, 8, 5, 10]
 
     def test_partition(self):
         linked_list = LinkedList(None)
         for value in self.values:
             linked_list.append_to_tail(value)
         linked_list.head = linked_list.head.next
-        linked_list = partition(linked_list, self.partition)
+        linked_list.head = partition(linked_list, self.partition)
 
         for value in self.expected:
-            self.assertEqual(value, linked_list.root.value)
-            linked_list.root = linked_list.root.next
+            self.assertEqual(value, linked_list.head.value)
+            linked_list.head = linked_list.head.next
 
 
 if __name__ == '__main__':
