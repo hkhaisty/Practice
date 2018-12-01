cdf743df52822a1516a4cb19c2eef1900f101029
Caleb_McCreary <clsmccreary@gmail.com>
2018-12-01 17:38:56 -0500

    Add queue

---
+++ CCI/Python3/Chapter3/queue.py
@@ -0,0 +1,38 @@
+class Queue:
+    class QueueNode:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+
+    def __init__(self):
+        self.first = Queue.QueueNode(None)
+        self.last = Queue.QueueNode(None)
+
+    def add(self, data):
+        node = Queue.QueueNode(data)
+        if self.last is not None:
+            self.last.next = node
+        self.last = node
+
+        if self.first is None:
+            self.first = self.last
+
+    def remove(self):
+        if self.first is None:
+            return None
+
+        data = self.first.data
+        self.first = self.first.next
+        if self.first is None:
+            self.last = None
+
+        return data
+
+    def peek(self):
+        if self.first is None:
+            return None
+
+        return self.first.data
+
+    def is_empty(self):
+        return self.first is None
