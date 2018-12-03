422c143248d911402ce06689f0c60652e395db7e
Caleb_McCreary <clsmccreary@gmail.com>
2018-12-02 22:16:19 -0500

    Add insertion

--- CCI/Python3/BitManipulation/insertion.py
+++ CCI/Python3/BitManipulation/insertion.py
@@ -1,10 +1,14 @@
 import unittest
 
 
-def insertion(M, N, i, j):
-    
+def insertion(m, n, i, j):
+    mask = (-1 << (j + 1)) | (1 << (i - 1))
+    return (n & mask) | (m << i)
+
 
 class Test(unittest.TestCase):
+    def test_insertion(self):
+        self.assertEqual(1100, insertion(19, 2**10, 2, 6))
 
 
 if __name__ == '__main__':
