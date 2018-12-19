90dd6a1d8903debfebbd5d599c46005ba7638a1f
Caleb McCreary <caleb@deltasigmacompany.com>
2018-12-19 16:11:37 -0500

    Add number of islands (#200) solution

---
+++ LeetCode/Python3/number_of_islands.py
@@ -0,0 +1,25 @@
+'''
+Solution -- Recursive Depth First Search
+Runtime Complexity -- O(m x n)
+Space Complexity -- O(m x n)
+'''
+def numIslands(self, grid):
+    def mark_landmass(grid, row, col):
+        if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[row]):
+            return 
+            
+        if grid[row][col] == '1':
+            grid[row][col] = '0'
+            mark_landmass(grid, row - 1, col)
+            mark_landmass(grid, row + 1, col)
+            mark_landmass(grid, row, col - 1)
+            mark_landmass(grid, row, col + 1)
+        
+    num_islands = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == '1':
+                num_islands += 1
+                mark_landmass(grid, row, col)
+                
+    return num_islands
