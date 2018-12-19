6c354f6be9621378cfb4f119bd0730f8de29a6cf
Caleb McCreary <caleb@deltasigmacompany.com>
2018-12-19 17:00:31 -0500

    Refactor to use enumerate starting feature

--- LeetCode/Python3/longest_substring_without_repeating_characters.py
+++ LeetCode/Python3/longest_substring_without_repeating_characters.py
@@ -6,10 +6,10 @@ Space Complexity - O(n), n = unique characters in s
 def lengthOfLongestSubstring(self, s):
     char_idx = {}
     result = last_repeated = 0
-    for i, c in enumerate(s):
+    for i, c in enumerate(s), 1:
         if c in char_idx:
             last_repeated = max(last_repeated, char_idx[c])               
-        char_idx[c] = i + 1     
-        result = max(result, char_idx[c] - last_repeated)
+        char_idx[c] = i  
+        result = max(result, i - last_repeated)
                 
     return result
