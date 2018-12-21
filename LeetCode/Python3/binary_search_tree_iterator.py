9632f4195bc413c963bc7694f625d9e9caede583
Caleb McCreary <caleb@deltasigmacompany.com>
2018-12-21 13:59:36 -0500

    Add #173

---
+++ LeetCode/Python3/binary_search_tree_iterator.py
@@ -0,0 +1,18 @@
+class BSTIterator(object):
+    def __init__(self, root):
+        self.nodes = []
+        while root:
+            self.nodes.append(root)
+            root = root.left
+
+    def hasNext(self):
+        return len(self.nodes) > 0
+
+    def next(self):
+        node = self.nodes.pop()
+        temp = node.right
+        while temp:
+            self.nodes.append(temp)
+            temp = temp.left
+            
+        return node.val
